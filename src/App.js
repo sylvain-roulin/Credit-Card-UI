bd01a41f47ff79bcc307461859c4feb8016bf341
Sylvain Roulin <hello@sylvainroulin.ch>
2019-05-19 12:02:43 +0200

    Pass to class-based components, add components folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './css/styles.min.css';
 
-function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        Hello World!
+      </div>
+    )
+  }
 }
 
 export default App;
