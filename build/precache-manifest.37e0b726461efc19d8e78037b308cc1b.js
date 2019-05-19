217af39bc7e30ec7f8ccd45d80498c1aa48b8bda
Sylvain Roulin <hello@sylvainroulin.ch>
2019-05-19 16:35:11 +0200

    Add .htaccess rule and homePage property for production

---
+++ build/precache-manifest.37e0b726461efc19d8e78037b308cc1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fada4a2798bee3954ad015377f00ee6",
+    "url": "/credit-card-ui/index.html"
+  },
+  {
+    "revision": "dd282725639e39448b39",
+    "url": "/credit-card-ui/static/css/main.e70d389e.chunk.css"
+  },
+  {
+    "revision": "9ed731d9b2fc0da67b19",
+    "url": "/credit-card-ui/static/js/2.509d01fc.chunk.js"
+  },
+  {
+    "revision": "dd282725639e39448b39",
+    "url": "/credit-card-ui/static/js/main.b4631c47.chunk.js"
+  },
+  {
+    "revision": "f5004f38e076cb1168ad",
+    "url": "/credit-card-ui/static/js/runtime~main.38177972.js"
+  },
+  {
+    "revision": "a282b72d9879b118c7c7608d535c6141",
+    "url": "/credit-card-ui/static/media/chip.a282b72d.png"
+  },
+  {
+    "revision": "f872c3f36f0a1fd23ce24de2533fba27",
+    "url": "/credit-card-ui/static/media/logo.f872c3f3.png"
+  }
+]);
